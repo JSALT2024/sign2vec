5a7ecddfeb48221bc99e010b7aac526503b28ede
karahan-sahin <karahan.sahin@boun.edu.tr>
2024-07-02 14:44:26 -0400

    change t5 encoder

--- finetune/t5.py
+++ finetune/t5.py
@@ -1,7 +1,16 @@
 
 import torch 
+import warnings
+from transformers.modeling_outputs import (
+    BaseModelOutput,
+    Seq2SeqLMOutput
+)
 from torch import nn
-from transformers import T5ForConditionalGeneration
+from transformers import (
+    T5ForConditionalGeneration
+)
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
+
 
 # 2. Define Custom T5 Model with Linear Layer
 class CustomT5Model(T5ForConditionalGeneration):
@@ -9,42 +18,134 @@ class CustomT5Model(T5ForConditionalGeneration):
         super().__init__(config)
         self.custom_linear = nn.Linear(162, config.d_model)  # Assuming input size is 128, adjust as needed
     
-    def forward(self, continuous_input=None, attention_mask=None, decoder_input_ids=None, 
-                decoder_attention_mask=None, labels=None):
+    def forward(self, 
+                continuous_input=None, 
+                attention_mask=None, 
+                encoder_outputs=None, 
+                decoder_input_ids=None, 
+                decoder_attention_mask=None,
+                head_mask=None,
+                decoder_head_mask=None,
+                output_attentions=None,
+                output_hidden_states=None,
+                encoder_head_mask=None,
+                decoder_inputs_embeds=None,
+                cross_attn_head_mask=None,
+                encoder_decoder_head_mask=None,
+                past_key_values=None,
+                inputs_embeds=None,
+                use_cache=None, 
+                return_dict=None,
+                labels=None):
         
         if continuous_input is not None:
             continuous_input_ = self.custom_linear(continuous_input)
             # generate attention mask for continuous input
-            encoder_outputs = self.encoder(
-                inputs_embeds=continuous_input_, 
-                attention_mask=attention_mask
-            )
-        else:
-            raise ValueError("continuous_input cannot be None")
-        
-        print("encoder_outputs: ", encoder_outputs)
-        print("encoder_outputs[0]: ", encoder_outputs[0].shape)
-        print("decoder_input_ids: ", decoder_input_ids.shape)
-        print("labels: ", labels.shape)
 
+        print("Continuous input:", continuous_input_.shape)
+
+        use_cache = use_cache if use_cache is not None else self.config.use_cache
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        # FutureWarning: head_mask was separated into two input args - head_mask, decoder_head_mask
+        if head_mask is not None and decoder_head_mask is None:
+            if self.config.num_layers == self.config.num_decoder_layers:
+                decoder_head_mask = head_mask
+
+
+        print("Decoder input ids:", decoder_input_ids)
+        # Encode if needed (training, first prediction pass)
+        if encoder_outputs is None:
+            # Convert encoder inputs in embeddings if needed
+            encoder_outputs = self.encoder(
+                input_ids=None,
+                attention_mask=attention_mask,
+                inputs_embeds=continuous_input_,
+                head_mask=head_mask,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
+            )
+            print("Encoder outputs:", encoder_outputs)
+        # Decode
+        elif return_dict and not isinstance(encoder_outputs, BaseModelOutput):
+            encoder_outputs = BaseModelOutput(
+                last_hidden_state=encoder_outputs[0],
+                hidden_states=encoder_outputs[1] if len(encoder_outputs) > 1 else None,
+                attentions=encoder_outputs[2] if len(encoder_outputs) > 2 else None,
+            )
+
+        hidden_states = encoder_outputs[0]
+
+        if self.model_parallel:
+            torch.cuda.set_device(self.decoder.first_device)
+
+        if labels is not None and decoder_input_ids is None and decoder_inputs_embeds is None:
+            # get decoder inputs from shifting lm labels to the right
+            decoder_input_ids = self._shift_right(labels)
+
+        # Set device for model parallelism
+        if self.model_parallel:
+            torch.cuda.set_device(self.decoder.first_device)
+            hidden_states = hidden_states.to(self.decoder.first_device)
+            if decoder_input_ids is not None:
+                decoder_input_ids = decoder_input_ids.to(self.decoder.first_device)
+            if attention_mask is not None:
+                attention_mask = attention_mask.to(self.decoder.first_device)
+            if decoder_attention_mask is not None:
+                decoder_attention_mask = decoder_attention_mask.to(self.decoder.first_device)
+
+        # Decode
         decoder_outputs = self.decoder(
             input_ids=decoder_input_ids,
             attention_mask=decoder_attention_mask,
-            encoder_hidden_states=encoder_outputs[0],  # encoder_outputs is a tuple, we need the first element
+            inputs_embeds=decoder_inputs_embeds,
+            past_key_values=past_key_values,
+            encoder_hidden_states=hidden_states,
             encoder_attention_mask=attention_mask,
-            use_cache=False,
+            head_mask=decoder_head_mask,
+            cross_attn_head_mask=cross_attn_head_mask,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
         )
 
         sequence_output = decoder_outputs[0]
-        logits = self.lm_head(sequence_output)
-        
+
+        # Set device for model parallelism
+        if self.model_parallel:
+            torch.cuda.set_device(self.encoder.first_device)
+            self.lm_head = self.lm_head.to(self.encoder.first_device)
+            sequence_output = sequence_output.to(self.lm_head.weight.device)
+
+        if self.config.tie_word_embeddings:
+            # Rescale output before projecting on vocab
+            # See https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/transformer/transformer.py#L586
+            sequence_output = sequence_output * (self.model_dim**-0.5)
+
+        lm_logits = self.lm_head(sequence_output)
+
         loss = None
         if labels is not None:
-            print("labels: ", labels.view(-1).shape)
-            print("logits: ", logits.view(-1, logits.size(-1)).shape)
-            loss_fct = nn.CrossEntropyLoss(ignore_index=-100)
-            loss = loss_fct(logits.view(-1, logits.size(-1)), labels.view(-1))
-            print("loss: ", loss)
+            loss_fct = CrossEntropyLoss(ignore_index=-100)
+            # move labels to correct device to enable PP
+            labels = labels.to(lm_logits.device)
+            loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), labels.view(-1))
+            # TODO(thom): Add z_loss https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/layers.py#L666
 
-    
-        return (loss, logits)
+        if not return_dict:
+            output = (lm_logits,) + decoder_outputs[1:] + encoder_outputs
+            return ((loss,) + output) if loss is not None else output
+
+        return Seq2SeqLMOutput(
+            loss=loss,
+            logits=lm_logits,
+            past_key_values=decoder_outputs.past_key_values,
+            decoder_hidden_states=decoder_outputs.hidden_states,
+            decoder_attentions=decoder_outputs.attentions,
+            cross_attentions=decoder_outputs.cross_attentions,
+            encoder_last_hidden_state=encoder_outputs.last_hidden_state,
+            encoder_hidden_states=encoder_outputs.hidden_states,
+            encoder_attentions=encoder_outputs.attentions,
+        )

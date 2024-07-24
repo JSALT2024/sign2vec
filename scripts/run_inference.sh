2d02f41cd82237839da6df1b7e59c063d3536199
karahan-sahin <karahan.sahin@boun.edu.tr>
2024-07-24 10:14:45 -0400

    add royal setup

--- scripts/run_inference.sh
+++ scripts/run_inference.sh
@@ -1,22 +1,22 @@
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2vec-yasl-k10-c128-d0.5 \
-                                                 --data_dir pretraining/how2sign \
-                                                 --output_path pretraining/how2sign \
+python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2vec-yasl-sc-sc-64-8-d1 \
+                                                 --data_dir /ssd1/karahan/ \
+                                                 --output_path /ssd1/karahan/ \
                                                  --input_file H2S_train.h5 \
                                                  --output_file sign2vec.train.0.h5 \
                                                  --metadata_file metadata_sign2vec.train.json \
                                                  --annotation_file sign2vec/how2sign/h2s.annotations.train.json
 
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2vec-yasl-k10-c128-d0.5 \
-                                                 --data_dir pretraining/how2sign \
-                                                 --output_path pretraining/how2sign \
+python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2vec-yasl-sc-sc-64-8-d1 \
+                                                 --data_dir /ssd1/karahan/ \
+                                                 --output_path /ssd1/karahan/ \
                                                  --input_file H2S_val.h5 \
                                                  --output_file sign2vec.dev.0.h5 \
                                                  --metadata_file metadata_sign2vec.dev.json \
                                                  --annotation_file sign2vec/how2sign/h2s.annotations.dev.json
 
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2vec-yasl-k10-c128-d0.5 \
-                                                 --data_dir pretraining/how2sign \
-                                                 --output_path pretraining/how2sign \
+python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2vec-yasl-sc-sc-64-8-d1 \
+                                                 --data_dir /ssd1/karahan/ \
+                                                 --output_path /ssd1/karahan/ \
                                                  --input_file H2S_test.h5 \
                                                  --output_file sign2vec.test.0.h5 \
                                                  --metadata_file metadata_sign2vec.test.json \

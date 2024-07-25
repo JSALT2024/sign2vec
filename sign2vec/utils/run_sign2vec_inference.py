cc9eeba57735fdd652277862c2d9ff940f15a8ae
karahan-sahin <karahan.sahin@boun.edu.tr>
2024-07-24 23:44:48 -0400

    iter

--- sign2vec/utils/run_sign2vec_inference.py
+++ sign2vec/utils/run_sign2vec_inference.py
@@ -146,7 +146,7 @@ if __name__ == '__main__':
             with h5py.File(shard, 'r') as f:
                 clips = list(f.keys())
                 for idx, clip in enumerate(clips):
-                    video_id = video_id.split('.')[0]
+                    video_id = clip.split('.')[0]
                     if current_video != video_id:
                         current_video = video_id
                         video_ids.append(current_video)

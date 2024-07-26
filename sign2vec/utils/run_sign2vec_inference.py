8b54749ca45e03a60db52b69e18d72e8e9ba3f35
karahan-sahin <karahan.sahin@boun.edu.tr>
2024-07-26 11:18:17 -0400

    chore: Update device selection in run_sign2vec_inference.py

--- sign2vec/utils/run_sign2vec_inference.py
+++ sign2vec/utils/run_sign2vec_inference.py
@@ -116,8 +116,8 @@ def save_to_h5(fetures_list_h5, label, index_dataset, chunk_batch, chunk_size):
 
 if __name__ == '__main__':
 
-    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   
-    device = torch.device('cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   
+    # device = torch.device('cpu')
 
     args = parse_args()
 

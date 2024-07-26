efec4600102df85092e971f7c270e3781f6f6e24
karahan-sahin <karahan.sahin@boun.edu.tr>
2024-07-26 18:04:12 -0400

    chore: Update pose inference script for How2Sign dataset

--- scripts/inference/run_pose_inference.sh
+++ scripts/inference/run_pose_inference.sh
@@ -6,7 +6,7 @@ rm -rf ${OUTPUT_DIR}/How2Sign/pose/pose.test.0.h5
 
 # mkdir -p ${OUTPUT_DIR}/How2Sign/
 
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2vec-yasl-mc-sc-64-2-d1 \
+python3 sign2vec/utils/run_pose_inference.py --model_name karahansahin/sign2vec-yasl-mc-sc-64-2-d1 \
                                                  --data_dir ${OUTPUT_DIR}/How2Sign \
                                                  --output_path ${OUTPUT_DIR}/How2Sign/pose \
                                                  --input_file H2S_train.h5 \
@@ -14,7 +14,7 @@ python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2
                                                  --metadata_file metadata_pose.train.json \
                                                  --annotation_file datasets/H2S.annotations.train.json
 
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2vec-yasl-mc-sc-64-2-d1 \
+python3 sign2vec/utils/run_pose_inference.py --model_name karahansahin/sign2vec-yasl-mc-sc-64-2-d1 \
                                                  --data_dir ${OUTPUT_DIR}/How2Sign \
                                                  --output_path ${OUTPUT_DIR}/How2Sign/pose \
                                                  --input_file H2S_val.h5 \
@@ -22,7 +22,7 @@ python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2
                                                  --metadata_file metadata_pose.dev.json \
                                                  --annotation_file datasets/H2S.annotations.dev.json
 
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2vec-yasl-mc-sc-64-2-d1 \
+python3 sign2vec/utils/run_pose_inference.py --model_name karahansahin/sign2vec-yasl-mc-sc-64-2-d1 \
                                                  --data_dir ${OUTPUT_DIR}/How2Sign \
                                                  --output_path ${OUTPUT_DIR}/How2Sign/pose \
                                                  --input_file H2S_test.h5 \

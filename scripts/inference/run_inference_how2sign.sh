56ef8a1a15b0255fbebed90e04bfd63d7e896ed9
karahan-sahin <karahan.sahin@boun.edu.tr>
2024-07-26 11:11:20 -0400

    iter

--- scripts/inference/run_inference_how2sign.sh
+++ scripts/inference/run_inference_how2sign.sh
@@ -12,7 +12,7 @@ python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2
                                                  --input_file H2S_train.h5 \
                                                  --output_file sign2vec.train.0.h5 \
                                                  --metadata_file metadata_sign2vec.train.json \
-                                                 --annotation_file sign2vec/how2sign/H2S.annotations.train.json
+                                                 --annotation_file datasets/H2S.annotations.train.json
 
 python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2vec-yasl-mc-sc-64-2-d1 \
                                                  --data_dir ${OUTPUT_DIR}/How2Sign \
@@ -20,7 +20,7 @@ python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2
                                                  --input_file H2S_val.h5 \
                                                  --output_file sign2vec.dev.0.h5 \
                                                  --metadata_file metadata_sign2vec.dev.json \
-                                                 --annotation_file sign2vec/how2sign/H2S.annotations.dev.json
+                                                 --annotation_file datasets/H2S.annotations.dev.json
 
 python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2vec-yasl-mc-sc-64-2-d1 \
                                                  --data_dir ${OUTPUT_DIR}/How2Sign \
@@ -28,5 +28,5 @@ python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2
                                                  --input_file H2S_test.h5 \
                                                  --output_file sign2vec.test.0.h5 \
                                                  --metadata_file metadata_sign2vec.test.json \
-                                                 --annotation_file sign2vec/how2sign/H2S.annotations.test.json
+                                                 --annotation_file datasets/H2S.annotations.test.json
 

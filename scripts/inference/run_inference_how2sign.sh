e6340b144beafad4a9f2e6616e55f57ccef4e83f
karahan-sahin <karahan.sahin@boun.edu.tr>
2024-07-26 11:07:12 -0400

    chore: Update output path for sign2vec inference script

--- scripts/inference/run_inference_how2sign.sh
+++ scripts/inference/run_inference_how2sign.sh
@@ -1,30 +1,32 @@
-rm -rf /ssd1/karahan/sign2vec.train.0.h5
-rm -rf /ssd1/karahan/sign2vec.dev.0.h5
-rm -rf /ssd1/karahan/sign2vec.test.0.h5
+export OUTPUT_DIR=sign2vec/data/how2sign
 
-mkdir -p /ssd1/karahan/How2Sign/
+rm -rf ${OUTPUT_DIR}/How2Sign/sign2vec/sign2vec.train.0.h5
+rm -rf ${OUTPUT_DIR}/How2Sign/sign2vec/sign2vec.dev.0.h5
+rm -rf ${OUTPUT_DIR}/How2Sign/sign2vec/sign2vec.test.0.h5
+
+mkdir -p ${OUTPUT_DIR}/How2Sign/
 
 python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2vec-yasl-mc-sc-64-2-d1 \
-                                                 --data_dir /ssd1/karahan/How2Sign \
-                                                 --output_path /ssd1/karahan/How2Sign/sign2vec \
+                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
+                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec \
                                                  --input_file H2S_train.h5 \
                                                  --output_file sign2vec.train.0.h5 \
                                                  --metadata_file metadata_sign2vec.train.json \
-                                                 --annotation_file sign2vec/how2sign/h2s.annotations.train.json
+                                                 --annotation_file sign2vec/how2sign/H2S.annotations.train.json
 
 python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2vec-yasl-mc-sc-64-2-d1 \
-                                                 --data_dir /ssd1/karahan/How2Sign \
-                                                 --output_path /ssd1/karahan/How2Sign/sign2vec \
+                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
+                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec \
                                                  --input_file H2S_val.h5 \
                                                  --output_file sign2vec.dev.0.h5 \
                                                  --metadata_file metadata_sign2vec.dev.json \
-                                                 --annotation_file sign2vec/how2sign/h2s.annotations.dev.json
+                                                 --annotation_file sign2vec/how2sign/H2S.annotations.dev.json
 
 python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2vec-yasl-mc-sc-64-2-d1 \
-                                                 --data_dir /ssd1/karahan/How2Sign \
-                                                 --output_path /ssd1/karahan/How2Sign/sign2vec \
+                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
+                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec \
                                                  --input_file H2S_test.h5 \
                                                  --output_file sign2vec.test.0.h5 \
                                                  --metadata_file metadata_sign2vec.test.json \
-                                                 --annotation_file sign2vec/how2sign/h2s.annotations.test.json
+                                                 --annotation_file sign2vec/how2sign/H2S.annotations.test.json
 

ad9eb3b4be8cecedf12191057b327e74b946036a
karahan-sahin <karahan.sahin@boun.edu.tr>
2024-07-28 02:05:38 -0400

    chore: Update pretraining configuration for Royal YouTube ASL dataset

--- scripts/pretraining/run_pretraining_yasl.sh
+++ scripts/pretraining/run_pretraining_yasl.sh
@@ -1,8 +1,5 @@
 TORCHDYNAMO_VERBOSE=1 accelerate launch pretraining/run_sign2vec_pretraining.py \
-                                                    --config_name="pretraining/training_configuration/royal/youtube_asl_mc_mc.yaml" 
+                                                    --config_name="pretraining/training_configuration/royal/youtube_asl_mc_sc_lconv.yaml" 
 
 TORCHDYNAMO_VERBOSE=1 accelerate launch pretraining/run_sign2vec_pretraining.py \
-                                                    --config_name="pretraining/training_configuration/royal/youtube_asl_mc_sc.yaml" 
-
-TORCHDYNAMO_VERBOSE=1 accelerate launch pretraining/run_sign2vec_pretraining.py \
-                                                    --config_name="pretraining/training_configuration/royal/youtube_asl_sc_sc.yaml" 
+                                                    --config_name="pretraining/training_configuration/royal/youtube_asl_sc_sc_lconv.yaml" 

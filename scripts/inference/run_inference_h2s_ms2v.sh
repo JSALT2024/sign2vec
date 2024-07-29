479625535841fe6ddbe26d7ee34cc962b98695d9
karahan-sahin <karahan.sahin@boun.edu.tr>
2024-07-28 22:52:02 -0400

    chore: Update run_sign2vec_inference.py to handle longer input features

--- scripts/inference/run_inference_h2s_ms2v.sh
+++ scripts/inference/run_inference_h2s_ms2v.sh
@@ -1,170 +1,6 @@
 export OUTPUT_DIR=/ssd1/karahan/
 
-export MODEL_NAME=sign2vec-yasl-mc-sc-64-2-d1
-export LAYER=3
-
-mkdir -p ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER}
-
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
-                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
-                                                 --input_file H2S_train.h5 \
-                                                 --output_file sign2vec.train.0.h5 \
-                                                 --metadata_file metadata_sign2vec.train.json \
-                                                 --annotation_file datasets/H2S.annotations.train.json \
-                                                 --layer_to_extract ${LAYER}
-
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
-                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
-                                                 --input_file H2S_val.h5 \
-                                                 --output_file sign2vec.dev.0.h5 \
-                                                 --metadata_file metadata_sign2vec.dev.json \
-                                                 --annotation_file datasets/H2S.annotations.dev.json \
-                                                 --layer_to_extract ${LAYER}
-
-
-export MODEL_NAME=sign2vec-yasl-mc-sc-64-2-d1
-export LAYER=6
-
-mkdir -p ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER}
-
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
-                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
-                                                 --input_file H2S_train.h5 \
-                                                 --output_file sign2vec.train.0.h5 \
-                                                 --metadata_file metadata_sign2vec.train.json \
-                                                 --annotation_file datasets/H2S.annotations.train.json \
-                                                 --layer_to_extract ${LAYER}
-
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
-                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
-                                                 --input_file H2S_val.h5 \
-                                                 --output_file sign2vec.dev.0.h5 \
-                                                 --metadata_file metadata_sign2vec.dev.json \
-                                                 --annotation_file datasets/H2S.annotations.dev.json \
-                                                 --layer_to_extract ${LAYER}
-
-
-export MODEL_NAME=sign2vec-yasl-mc-sc-64-2-d1
-export LAYER=9
-
-mkdir -p ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER}
-
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
-                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
-                                                 --input_file H2S_train.h5 \
-                                                 --output_file sign2vec.train.0.h5 \
-                                                 --metadata_file metadata_sign2vec.train.json \
-                                                 --annotation_file datasets/H2S.annotations.train.json \
-                                                 --layer_to_extract ${LAYER}
-
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
-                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
-                                                 --input_file H2S_val.h5 \
-                                                 --output_file sign2vec.dev.0.h5 \
-                                                 --metadata_file metadata_sign2vec.dev.json \
-                                                 --annotation_file datasets/H2S.annotations.dev.json \
-                                                 --layer_to_extract ${LAYER}
-
-
-export MODEL_NAME=karahansahin/sign2vec-yasl-mc-sc-64-2-d1-warmup-decay
-export LAYER=3
-
-mkdir -p ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER}
-
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
-                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
-                                                 --input_file H2S_train.h5 \
-                                                 --output_file sign2vec.train.0.h5 \
-                                                 --metadata_file metadata_sign2vec.train.json \
-                                                 --annotation_file datasets/H2S.annotations.train.json \
-                                                 --layer_to_extract ${LAYER}
-
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
-                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
-                                                 --input_file H2S_val.h5 \
-                                                 --output_file sign2vec.dev.0.h5 \
-                                                 --metadata_file metadata_sign2vec.dev.json \
-                                                 --annotation_file datasets/H2S.annotations.dev.json \
-                                                 --layer_to_extract ${LAYER}
-
-export LAYER=3
-
-mkdir -p ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER}
-
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
-                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
-                                                 --input_file H2S_train.h5 \
-                                                 --output_file sign2vec.train.0.h5 \
-                                                 --metadata_file metadata_sign2vec.train.json \
-                                                 --annotation_file datasets/H2S.annotations.train.json \
-                                                 --layer_to_extract ${LAYER}
-
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
-                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
-                                                 --input_file H2S_val.h5 \
-                                                 --output_file sign2vec.dev.0.h5 \
-                                                 --metadata_file metadata_sign2vec.dev.json \
-                                                 --annotation_file datasets/H2S.annotations.dev.json \
-                                                 --layer_to_extract ${LAYER}
-
-
-export LAYER=6
-
-mkdir -p ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER}
-
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
-                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
-                                                 --input_file H2S_train.h5 \
-                                                 --output_file sign2vec.train.0.h5 \
-                                                 --metadata_file metadata_sign2vec.train.json \
-                                                 --annotation_file datasets/H2S.annotations.train.json \
-                                                 --layer_to_extract ${LAYER}
-
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
-                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
-                                                 --input_file H2S_val.h5 \
-                                                 --output_file sign2vec.dev.0.h5 \
-                                                 --metadata_file metadata_sign2vec.dev.json \
-                                                 --annotation_file datasets/H2S.annotations.dev.json \
-                                                 --layer_to_extract ${LAYER}
-
-
-export LAYER=9
-
-mkdir -p ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER}
-
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
-                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
-                                                 --input_file H2S_train.h5 \
-                                                 --output_file sign2vec.train.0.h5 \
-                                                 --metadata_file metadata_sign2vec.train.json \
-                                                 --annotation_file datasets/H2S.annotations.train.json \
-                                                 --layer_to_extract ${LAYER}
-
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
-                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
-                                                 --input_file H2S_val.h5 \
-                                                 --output_file sign2vec.dev.0.h5 \
-                                                 --metadata_file metadata_sign2vec.dev.json \
-                                                 --annotation_file datasets/H2S.annotations.dev.json \
-                                                 --layer_to_extract ${LAYER}
-
-
-export MODEL_NAME=karahansahin/sign2vec-yasl-mc-mc-64-2-d1-decay
+export MODEL_NAME=sign2vec-yasl-mc-sc-64-2-d1-warmup-decay
 export LAYER=-1
 
 mkdir -p ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER}
@@ -256,7 +92,99 @@ python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MOD
                                                  --layer_to_extract ${LAYER}
 
 
-export MODEL_NAME=karahansahin/sign2vec-yasl-sc-sc-80-4-d1-decay
+export MODEL_NAME=sign2vec-yasl-mc-mc-64-2-d1-decay
+export LAYER=-1
+
+mkdir -p ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER}
+
+python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
+                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
+                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
+                                                 --input_file H2S_train.h5 \
+                                                 --output_file sign2vec.train.0.h5 \
+                                                 --metadata_file metadata_sign2vec.train.json \
+                                                 --annotation_file datasets/H2S.annotations.train.json \
+                                                 --layer_to_extract ${LAYER}
+
+python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
+                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
+                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
+                                                 --input_file H2S_val.h5 \
+                                                 --output_file sign2vec.dev.0.h5 \
+                                                 --metadata_file metadata_sign2vec.dev.json \
+                                                 --annotation_file datasets/H2S.annotations.dev.json \
+                                                 --layer_to_extract ${LAYER}
+
+export LAYER=3
+
+mkdir -p ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER}
+
+python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
+                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
+                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
+                                                 --input_file H2S_train.h5 \
+                                                 --output_file sign2vec.train.0.h5 \
+                                                 --metadata_file metadata_sign2vec.train.json \
+                                                 --annotation_file datasets/H2S.annotations.train.json \
+                                                 --layer_to_extract ${LAYER}
+
+python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
+                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
+                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
+                                                 --input_file H2S_val.h5 \
+                                                 --output_file sign2vec.dev.0.h5 \
+                                                 --metadata_file metadata_sign2vec.dev.json \
+                                                 --annotation_file datasets/H2S.annotations.dev.json \
+                                                 --layer_to_extract ${LAYER}
+
+
+export LAYER=6
+
+mkdir -p ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER}
+
+python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
+                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
+                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
+                                                 --input_file H2S_train.h5 \
+                                                 --output_file sign2vec.train.0.h5 \
+                                                 --metadata_file metadata_sign2vec.train.json \
+                                                 --annotation_file datasets/H2S.annotations.train.json \
+                                                 --layer_to_extract ${LAYER}
+
+python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
+                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
+                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
+                                                 --input_file H2S_val.h5 \
+                                                 --output_file sign2vec.dev.0.h5 \
+                                                 --metadata_file metadata_sign2vec.dev.json \
+                                                 --annotation_file datasets/H2S.annotations.dev.json \
+                                                 --layer_to_extract ${LAYER}
+
+
+export LAYER=9
+
+mkdir -p ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER}
+
+python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
+                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
+                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
+                                                 --input_file H2S_train.h5 \
+                                                 --output_file sign2vec.train.0.h5 \
+                                                 --metadata_file metadata_sign2vec.train.json \
+                                                 --annotation_file datasets/H2S.annotations.train.json \
+                                                 --layer_to_extract ${LAYER}
+
+python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
+                                                 --data_dir ${OUTPUT_DIR}/How2Sign \
+                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
+                                                 --input_file H2S_val.h5 \
+                                                 --output_file sign2vec.dev.0.h5 \
+                                                 --metadata_file metadata_sign2vec.dev.json \
+                                                 --annotation_file datasets/H2S.annotations.dev.json \
+                                                 --layer_to_extract ${LAYER}
+
+
+export MODEL_NAME=sign2vec-yasl-sc-sc-80-4-d1-decay
 export LAYER=-1
 
 mkdir -p ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER}
--- scripts/inference/run_inference_how2sign.sh
+++ scripts/inference/run_inference_how2sign.sh
@@ -1,32 +1,36 @@
 export OUTPUT_DIR=/ssd1/karahan/
 
-rm -rf ${OUTPUT_DIR}/How2Sign/sign2vec/sign2vec.train.0.h5
-rm -rf ${OUTPUT_DIR}/How2Sign/sign2vec/sign2vec.dev.0.h5
-rm -rf ${OUTPUT_DIR}/How2Sign/sign2vec/sign2vec.test.0.h5
-
 mkdir -p ${OUTPUT_DIR}/How2Sign/
 
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2vec-yasl-mc-sc-64-2-d1 \
+export MODEL_NAME=sign2vec-yasl-mc-sc-64-2-d1
+export LAYER=-1
+
+mkdir -p ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER}
+
+python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
                                                  --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec \
+                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
                                                  --input_file H2S_train.h5 \
                                                  --output_file sign2vec.train.0.h5 \
                                                  --metadata_file metadata_sign2vec.train.json \
-                                                 --annotation_file datasets/H2S.annotations.train.json
+                                                 --annotation_file datasets/H2S.annotations.train.json \
+                                                 --layer_to_extract -1
 
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2vec-yasl-mc-sc-64-2-d1 \
+python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
                                                  --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec \
+                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
                                                  --input_file H2S_val.h5 \
                                                  --output_file sign2vec.dev.0.h5 \
                                                  --metadata_file metadata_sign2vec.dev.json \
-                                                 --annotation_file datasets/H2S.annotations.dev.json
+                                                 --annotation_file datasets/H2S.annotations.dev.json \
+                                                 --layer_to_extract -1
 
-python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/sign2vec-yasl-mc-sc-64-2-d1 \
+python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
                                                  --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec \
+                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
                                                  --input_file H2S_test.h5 \
                                                  --output_file sign2vec.test.0.h5 \
                                                  --metadata_file metadata_sign2vec.test.json \
-                                                 --annotation_file datasets/H2S.annotations.test.json
+                                                 --annotation_file datasets/H2S.annotations.test.json\
+                                                 --layer_to_extract -1
 

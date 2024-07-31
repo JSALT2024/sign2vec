607a89818a6ad2ba4fe17d4dc26a70f397e5e1de
karahan-sahin <karahan.sahin@boun.edu.tr>
2024-07-31 11:09:52 -0400

    chore: Update run_inference_how2sign.sh to use local variable for MODEL_NAME and LAYER

--- scripts/inference/run_inference_how2sign.sh
+++ scripts/inference/run_inference_how2sign.sh
@@ -2,14 +2,14 @@ export OUTPUT_DIR=/ssd1/karahan/
 
 mkdir -p ${OUTPUT_DIR}/How2Sign/
 
-MODEL_NAME=sign2vec-yasl-mc-sc-lconv-128
+MODEL_NAME=karahansahin/sign2vec-yasl-sc-sc-64-8-d1
 
 LAYER=-1
 mkdir -p ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER}
 
 python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
                                                  --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
+                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/ \
                                                  --input_file H2S_train.h5 \
                                                  --output_file sign2vec.train.0.h5 \
                                                  --metadata_file metadata_sign2vec.train.json \
@@ -18,7 +18,7 @@ python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MOD
 
 python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
                                                  --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
+                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/ \
                                                  --input_file H2S_val.h5 \
                                                  --output_file sign2vec.dev.0.h5 \
                                                  --metadata_file metadata_sign2vec.dev.json \
@@ -27,7 +27,7 @@ python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MOD
 
 python3 sign2vec/utils/run_sign2vec_inference.py --model_name karahansahin/${MODEL_NAME} \
                                                  --data_dir ${OUTPUT_DIR}/How2Sign \
-                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/${MODEL_NAME}_layer=${LAYER} \
+                                                 --output_path ${OUTPUT_DIR}/How2Sign/sign2vec/ \
                                                  --input_file H2S_test.h5 \
                                                  --output_file sign2vec.test.0.h5 \
                                                  --metadata_file metadata_sign2vec.test.json \

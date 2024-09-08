2b45ae90a0e291c5d07b80c7e3730731cceff202
karahan-sahin <karahan.sahin@boun.edu.tr>
2024-09-08 18:03:22 +0300

    Add support for CUDA in model training, update sentence length in H5 files, and remove unnecessary files and code

--- sign2vec/train/run_finetuning.py
+++ sign2vec/train/run_finetuning.py
@@ -6,7 +6,8 @@ from transformers import (
     Seq2SeqTrainingArguments,
     Seq2SeqTrainer,
     T5Tokenizer, 
-    T5Config
+    T5Config,
+    Adafactor
 )
 
 from sign2vec.model.t5 import T5ModelForSLT
@@ -32,16 +33,20 @@ def parse_args():
 
     # Training arguments
     parser.add_argument("--model_id", type=str, default="t5-small")
-    parser.add_argument("--num_train_epochs", type=int, default=20)
+    parser.add_argument("--num_train_epochs", type=int, default=2)
+    parser.add_argument("--eval_steps", type=int, default=5)
     parser.add_argument("--per_device_train_batch_size", type=int, default=16)
     parser.add_argument("--per_device_eval_batch_size", type=int, default=16)
     parser.add_argument("--gradient_accumulation_steps", type=float, default=4)
-    parser.add_argument("--learning_rate", type=float, default=2e-5)
+    parser.add_argument("--learning_rate", type=float, default=0.001)
     parser.add_argument("--weight_decay", type=float, default=0.01)
     parser.add_argument("--fp16", action="store_true")
     parser.add_argument("--push_to_hub", action="store_true")
     parser.add_argument("--report_to", type=str, default=None)
     parser.add_argument("--logging_steps", type=int, default=100)
+
+    # Running arguments
+    parser.add_argument("--dev", action="store_true")
 
     return parser.parse_args()
 
@@ -60,7 +65,10 @@ if __name__ == "__main__":
 
     # Import How2SignForSLT dataset
     h2s_train = How2SignForSLT(
-        h5_fpath=os.path.join(args.h2s_dir, 'H2S_train.h5'),   
+        h5_fpath=os.path.join(
+            args.h2s_dir, 
+            'H2S_train.h5' if not args.dev else 'H2S_test.h5'
+        ),   
         transform=None,
         max_token_length=args.max_token_length,
         max_sequence_length=args.max_sequence_length,
@@ -69,7 +77,10 @@ if __name__ == "__main__":
     )
 
     h2s_val = How2SignForSLT(
-        h5_fpath=os.path.join(args.h2s_dir, 'H2S_val.h5'),   
+        h5_fpath=os.path.join(
+            args.h2s_dir, 
+            'H2S_val.h5' if not args.dev else 'H2S_test.h5'
+        ),   
         transform=None,
         max_token_length=args.max_token_length,
         max_sequence_length=args.max_sequence_length,
@@ -87,14 +98,16 @@ if __name__ == "__main__":
         tokenizer=args.model_id
     )
 
-    metric = evaluate.load('sacrebleu')
+    sacrebleu = evaluate.load('sacrebleu')
 
     def compute_metrics(eval_preds):
         preds, labels = eval_preds
 
         if isinstance(preds, tuple):
             preds = preds[0]
+            print('Preds:', preds.shape)
             preds = np.argmax(preds, axis=2)
+            print('Preds after:', preds.shape)
 
         decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
 
@@ -103,30 +116,42 @@ if __name__ == "__main__":
 
         decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
     
-        for i in range(20):
+        for i in range(10):
             print(f"Prediction: {decoded_preds[i]}")
             print(f"Reference: {decoded_labels[i]}")
             print('*'*50)
 
-        result = metric.compute(predictions=decoded_preds, references=decoded_labels)
-        result = {"bleu": result["score"]}
+        result = sacrebleu.compute(predictions=decoded_preds, references=decoded_labels)
+        result = {
+            "bleu": result["score"], 
+            'bleu-1': result['precisions'][0],
+            'bleu-2': result['precisions'][1],
+            'bleu-3': result['precisions'][2],
+        }
 
         prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
         result["gen_len"] = np.mean(prediction_lens)
         result = {k: round(v, 4) for k, v in result.items()}
+
         return result
     
+
     # Configure the wandb logger
     os.environ["WANDB_PROJECT"] = "h2s-t5"
     os.environ["WANDB_NAME"] = args.model_name
     os.environ["WANDB_LOG_MODEL"] = "true"
+    os.environ["WANDB_WATCH"] = "all"
+
+    os.environ["WANDB_DISABLED"] = "true" if not args.dev else "false"
+
 
     # Add model and data to device
     model.to("cuda")
 
     training_args = Seq2SeqTrainingArguments(
         output_dir=args.output_dir,
-        evaluation_strategy="epoch",
+        evaluation_strategy="steps",
+        eval_steps=args.eval_steps,
         logging_steps=args.logging_steps,
         num_train_epochs=args.num_train_epochs,
         per_device_train_batch_size=args.per_device_train_batch_size,
@@ -138,6 +163,8 @@ if __name__ == "__main__":
         push_to_hub=args.push_to_hub,
         report_to=args.report_to,
         hub_model_id=f"{args.model_name}",
+        metric_for_best_model="bleu",
+        optim="adafactor",
     )
 
     print('Model device:', training_args.device)
@@ -152,13 +179,24 @@ if __name__ == "__main__":
         compute_metrics=compute_metrics,
     )
 
-
     trainer.train()
 
-    # Evaluate the model
-    eval_results = trainer.evaluate(eval_dataset=h2s_test)
-    print(eval_results)
-
+    # Run inference on the test set
+    (logits, _ ), label_ids, eval_results = trainer.predict(test_dataset=h2s_test)
+    # Decode the predictions
+    predicted_ids = np.argmax(logits, axis=2)
+    decoded_preds = tokenizer.batch_decode(predicted_ids, skip_special_tokens=True)
+    # Save the predictions and original sentences to a file
+    import json
+    with open(os.path.join(args.output_dir, f"{args.model_name}_test_predictions.json"), "w") as f:
+        predictions = []
+        for idx, pred in enumerate(decoded_preds):
+            predictions.append({
+                'ground_truth': h2s_test[idx]['sentence'],
+                'prediction': pred
+            })
+        json.dump(predictions, f, indent=4)
+        
     # Save evaluation results to json
     with open(os.path.join(args.output_dir, f"{args.model_name}_eval_results.json"), "w") as f:
-        f.write(str(eval_results))
+        json.dump(eval_results, f, indent=4)

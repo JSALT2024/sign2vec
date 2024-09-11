4f711f7c186c6f534b212a33aef2fa0d933feaf0
karahan-sahin <karahan.sahin@boun.edu.tr>
2024-09-11 15:04:35 +0300

    fix

--- sign2vec/train/run_sign2vec_pretraining.py
+++ sign2vec/train/run_sign2vec_pretraining.py
@@ -500,15 +500,14 @@ def main():
             h5_fpath=args.dataset_path,
             max_sequence_length=max_length,
             transform=None,
-            mode='train',
+            mode=args.datasets[0],
         )
 
         eval_dataset = YoutubeASLForSign2VecPretraining(
             h5_fpath=args.dataset_path,
-            csv_file_path=args.dataset_path,
             max_sequence_length=max_length,
             transform=None,
-            mode='test'
+            mode=args.datasets[1]
         )
     else:
         raise ValueError(f"Invalid dataset name: {args.dataset_name}")

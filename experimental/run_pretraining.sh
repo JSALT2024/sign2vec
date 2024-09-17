e7208fcc28b1643fec4f94bbd9317bf79d0c9278
karahan-sahin <karahan.sahin@boun.edu.tr>
2024-09-17 15:28:04 +0300

    Add run_pretraining.sh script for Sign2Vec pretraining with specified parameters

---
+++ experimental/run_pretraining.sh
@@ -0,0 +1,25 @@
+TORCHDYNAMO_VERBOSE=1 accelerate launch sign2vec/train/run_sign2vec_pretraining.py \
+                    --dataset_name="YoutubeASL" \
+                    --tags sign2vec base no-norm single_cue \
+                    --datasets "train" "test" \
+                    --dataset_path="/ssd2/karahan/YASL/pose"  \
+                    --model_config_file="experimental/configs/sign2vec_pretraining_config.yaml" \
+                    --output_dir="./sign2vec-base-v0.0" \
+                    --max_train_steps="20000" \
+                    --num_warmup_steps="32000" \
+                    --gradient_accumulation_steps="4" \
+                    --learning_rate="0.005" \
+                    --weight_decay="0.01" \
+                    --max_duration_in_seconds="20.0" \
+                    --min_duration_in_seconds="2.0" \
+                    --logging_steps="1" \
+                    --saving_steps="5000" \
+                    --per_device_train_batch_size="32" \
+                    --per_device_eval_batch_size="32" \
+                    --adam_beta1="0.9" \
+                    --adam_beta2="0.98" \
+                    --adam_epsilon="1e-06" \
+                    --gradient_checkpointing \
+                    --mask_time_prob="0.65" \
+                    --mask_time_length="10" \
+                    --push_to_hub 

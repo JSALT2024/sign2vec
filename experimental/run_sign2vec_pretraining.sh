6618d9cd225478677563f0b9d9f38d55ef669f87
karahan-sahin <karahan.sahin@boun.edu.tr>
2024-11-10 19:51:49 +0300

    Update run_sign2vec_pretraining.sh to modify run names and output directories for consistency; add new configurations for mixed, linear, and convolutional models with specific parameters.

--- experimental/run_sign2vec_pretraining.sh
+++ experimental/run_sign2vec_pretraining.sh
@@ -5,13 +5,13 @@ export CUDA_VISIBLE_DEVICES=$cuda_device
 
 TORCHDYNAMO_VERBOSE=1 accelerate launch sign2vec/train/run_sign2vec_pretraining.py \
                     --dataset_name="YoutubeASL" \
-                    --run_name="sign2vec-base-mixed-zero-mean" \
-                    --hub_model_id="sign2vec-base-mixed-zero-mean" \
+                    --run_name="sign2vec-base-mixed-zero-mean-div-1.0" \
+                    --hub_model_id="sign2vec-base-mixed-zero-mean-div-1.0" \
                     --tags sign2vec base yasl-norm single_cue \
                     --datasets "train" "test" \
                     --dataset_path="/ssd2/karahan/YASL/pose"  \
                     --model_config_file="experimental/configs/s2v_multi_code_mixed.yaml" \
-                    --output_dir="/ssd2/karahan/YASL/sign2vec-base-mixed-zero-mean" \
+                    --output_dir="/ssd2/karahan/YASL/sign2vec-base-mixed-zero-mean-div-1.0" \
                     --max_train_steps="10000" \
                     --num_warmup_steps="1000" \
                     --gradient_accumulation_steps="8" \
@@ -34,13 +34,13 @@ TORCHDYNAMO_VERBOSE=1 accelerate launch sign2vec/train/run_sign2vec_pretraining.
 
 TORCHDYNAMO_VERBOSE=1 accelerate launch sign2vec/train/run_sign2vec_pretraining.py \
                     --dataset_name="YoutubeASL" \
-                    --run_name="sign2vec-base-linear-zero-mean" \
-                    --hub_model_id="sign2vec-base-linear-zero-mean" \
+                    --run_name="sign2vec-base-linear-zero-mean-div-1.0" \
+                    --hub_model_id="sign2vec-base-linear-zero-mean-div-1.0" \
                     --tags sign2vec base yasl-norm single_cue \
                     --datasets "train" "test" \
                     --dataset_path="/ssd2/karahan/YASL/pose"  \
                     --model_config_file="experimental/configs/s2v_multi_code_linear.yaml" \
-                    --output_dir="/ssd2/karahan/YASL/sign2vec-base-linear-zero-mean" \
+                    --output_dir="/ssd2/karahan/YASL/sign2vec-base-linear-zero-mean-div-1.0" \
                     --max_train_steps="10000" \
                     --num_warmup_steps="1000" \
                     --gradient_accumulation_steps="8" \
@@ -63,13 +63,101 @@ TORCHDYNAMO_VERBOSE=1 accelerate launch sign2vec/train/run_sign2vec_pretraining.
 
 TORCHDYNAMO_VERBOSE=1 accelerate launch sign2vec/train/run_sign2vec_pretraining.py \
                     --dataset_name="YoutubeASL" \
-                    --run_name="sign2vec-base-conv-zero-mean" \
-                    --hub_model_id="sign2vec-base-conv-zero-mean" \
+                    --run_name="sign2vec-base-conv-zero-mean-div-1.0" \
+                    --hub_model_id="sign2vec-base-conv-zero-mean-div-1.0" \
                     --tags sign2vec base yasl-norm single_cue \
                     --datasets "train" "test" \
                     --dataset_path="/ssd2/karahan/YASL/pose"  \
                     --model_config_file="experimental/configs/s2v_multi_code_conv.yaml" \
-                    --output_dir="/ssd2/karahan/YASL/sign2vec-base-conv-zero-mean" \
+                    --output_dir="/ssd2/karahan/YASL/sign2vec-base-conv-zero-mean-div-1.0" \
+                    --max_train_steps="10000" \
+                    --num_warmup_steps="1000" \
+                    --gradient_accumulation_steps="8" \
+                    --learning_rate="0.0005" \
+                    --weight_decay="0.001" \
+                    --max_duration_in_seconds="20.0" \
+                    --min_duration_in_seconds="2.0" \
+                    --logging_steps="1" \
+                    --saving_steps="5000" \
+                    --per_device_train_batch_size="16" \
+                    --per_device_eval_batch_size="16" \
+                    --adam_beta1="0.9" \
+                    --adam_beta2="0.98" \
+                    --adam_epsilon="1e-06" \
+                    --gradient_checkpointing \
+                    --mask_time_prob="0.65" \
+                    --mask_time_length="10" \
+                    --zero_mean \
+                    --push_to_hub 
+
+
+TORCHDYNAMO_VERBOSE=1 accelerate launch sign2vec/train/run_sign2vec_pretraining.py \
+                    --dataset_name="YoutubeASL" \
+                    --run_name="sign2vec-base-mixed-zero-mean-div-1.0-c128" \
+                    --hub_model_id="sign2vec-base-mixed-zero-mean-div-1.0-c128" \
+                    --tags sign2vec base yasl-norm single_cue \
+                    --datasets "train" "test" \
+                    --dataset_path="/ssd2/karahan/YASL/pose"  \
+                    --model_config_file="experimental/configs/s2v_multi_code_mixed_n.yaml" \
+                    --output_dir="/ssd2/karahan/YASL/sign2vec-base-mixed-zero-mean-div-1.0-c128" \
+                    --max_train_steps="10000" \
+                    --num_warmup_steps="1000" \
+                    --gradient_accumulation_steps="8" \
+                    --learning_rate="0.0005" \
+                    --weight_decay="0.001" \
+                    --max_duration_in_seconds="20.0" \
+                    --min_duration_in_seconds="2.0" \
+                    --logging_steps="1" \
+                    --saving_steps="5000" \
+                    --per_device_train_batch_size="16" \
+                    --per_device_eval_batch_size="16" \
+                    --adam_beta1="0.9" \
+                    --adam_beta2="0.98" \
+                    --adam_epsilon="1e-06" \
+                    --gradient_checkpointing \
+                    --mask_time_prob="0.65" \
+                    --mask_time_length="10" \
+                    --zero_mean \
+                    --push_to_hub 
+
+TORCHDYNAMO_VERBOSE=1 accelerate launch sign2vec/train/run_sign2vec_pretraining.py \
+                    --dataset_name="YoutubeASL" \
+                    --run_name="sign2vec-base-linear-zero-mean-div-1.0-c128" \
+                    --hub_model_id="sign2vec-base-linear-zero-mean-div-1.0-c128" \
+                    --tags sign2vec base yasl-norm single_cue \
+                    --datasets "train" "test" \
+                    --dataset_path="/ssd2/karahan/YASL/pose"  \
+                    --model_config_file="experimental/configs/s2v_multi_code_linear_n.yaml" \
+                    --output_dir="/ssd2/karahan/YASL/sign2vec-base-linear-zero-mean-div-1.0-c128" \
+                    --max_train_steps="10000" \
+                    --num_warmup_steps="1000" \
+                    --gradient_accumulation_steps="8" \
+                    --learning_rate="0.0005" \
+                    --weight_decay="0.001" \
+                    --max_duration_in_seconds="20.0" \
+                    --min_duration_in_seconds="2.0" \
+                    --logging_steps="1" \
+                    --saving_steps="5000" \
+                    --per_device_train_batch_size="16" \
+                    --per_device_eval_batch_size="16" \
+                    --adam_beta1="0.9" \
+                    --adam_beta2="0.98" \
+                    --adam_epsilon="1e-06" \
+                    --gradient_checkpointing \
+                    --mask_time_prob="0.65" \
+                    --mask_time_length="10" \
+                    --zero_mean \
+                    --push_to_hub 
+
+TORCHDYNAMO_VERBOSE=1 accelerate launch sign2vec/train/run_sign2vec_pretraining.py \
+                    --dataset_name="YoutubeASL" \
+                    --run_name="sign2vec-base-conv-zero-mean-div-1.0-c128" \
+                    --hub_model_id="sign2vec-base-conv-zero-mean-div-1.0-c128" \
+                    --tags sign2vec base yasl-norm single_cue \
+                    --datasets "train" "test" \
+                    --dataset_path="/ssd2/karahan/YASL/pose"  \
+                    --model_config_file="experimental/configs/s2v_multi_code_conv_n.yaml" \
+                    --output_dir="/ssd2/karahan/YASL/sign2vec-base-conv-zero-mean-div-1.0-c128" \
                     --max_train_steps="10000" \
                     --num_warmup_steps="1000" \
                     --gradient_accumulation_steps="8" \
